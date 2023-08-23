7bf10c79a0b210319188e1731782c286228b0b9e
Roland Fredenhagen <dev@modprog.de>
2023-08-23 11:14:30 +0200

    Support stdin and multiple files

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,30 @@
-use std::fs;
+use std::io::Read;
 use std::path::PathBuf;
+use std::{fs, io};
 
+use anyhow::{anyhow, Error, Result};
 use clap::Parser;
 use ron_edit::File;
 
 #[derive(Parser)]
 struct Opts {
-    file: PathBuf,
+    files: Option<Vec<PathBuf>>,
 }
 
-fn main() {
+fn main() -> Result<()> {
     let opts = Opts::parse();
-    let ron = fs::read_to_string(&opts.file).unwrap();
-    let ron = File::try_from(ron.as_str()).unwrap();
-    fs::write(opts.file, ronda::format(&ron)).unwrap();
+    if let Some(files) = &opts.files {
+        for file in files {
+            let ron = fs::read_to_string(file)?;
+            let ron = File::try_from(ron.as_str())
+                .map_err(|err| anyhow!("Error while reading `{}`: {err}", file.display()))?;
+            fs::write(file, ronda::format(&ron))?;
+        }
+    } else {
+        let mut ron = String::new();
+        io::stdin().read_to_string(&mut ron)?;
+        let ron = File::try_from(ron.as_str()).map_err(Error::msg)?;
+        print!("{}", ronda::format(&ron));
+    }
+    Ok(())
 }
